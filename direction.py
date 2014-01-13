59423643b165a58f7246e40b55daf130a736e373
Carl-Oscar Erneholm <co.erneholm@gmail.com>
2014-01-13 22:36:01 +0100

    Fixed walk around bug.

--- direction.py
+++ direction.py
@@ -7,10 +7,10 @@ DOWN = (0, 1)
 RIGHT = (1, 0)
 LEFT = (-1, 0)
 
-UP_LEFT = (-1, 1)
-UP_RIGHT = (1, 1)
-DOWN_RIGHT = (1, -1)
-DOWN_LEFT = (-1, -1)
+UP_LEFT = (-1, -1)
+UP_RIGHT = (1, -1)
+DOWN_RIGHT = (1, 1)
+DOWN_LEFT = (-1, 1)
 
 DIRECTIONS = [UP,
               UP_RIGHT,
